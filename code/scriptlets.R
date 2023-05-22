2f610e323703f9cee9ebb1cd5c85061121194fa8
Mehadi Hasan <ngshasan@gmail.com>
2023-05-22 01:21:56 -0500

    Refactor code to move some functions into utilities file

--- code/scriptlets.R
+++ code/scriptlets.R
@@ -35,6 +35,26 @@ for (trials in 1:3000) {
   print(paste(trials, ": ", (count*4) / trials))
 }
 
+# This script will include a collection of small scripts steps
+# often seen as example code. We're using this solely for demo purposes
+
+# Put globals, installs, and sources here
+source("./util_functions.R")
+
+# Put functions here
+#   1. Prints hello world
+hello_world <- function() {
+  myString <- "Hello, World!"
+  print (myString)
+}
+
+# main code
+
+hello_world()
+
+square_it(10)
+
+montecarloPi(3000)
 
 # END
 
